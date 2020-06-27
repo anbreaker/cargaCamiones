ce8848ffed7ad117f47065ee6219bed875a7f354
anbreaker <antunez19@gmail.com>
2020-06-27 17:27:27 +0200

    calculo cisterna terminado

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Peso');
+const pesoCisternaTotal = 40800;
+
+function calculoCisternas(tara) {
+  const carga = pesoCisternaTotal - tara;
+  let divisionCarga = carga / 2;
+
+  if (divisionCarga % 20 !== 0) {
+    divisionCarga = divisionCarga + 10;
+    // console.log('divisionCargaDespues -->', divisionCarga);
+    return divisionCarga;
+  } else {
+    return divisionCarga;
+    // console.log('divisionCargaDespues -->', divisionCarga);
+  }
+}
+
+// calculoCisternas(14160);
+console.log('Cargar -->', calculoCisternas(14960));
